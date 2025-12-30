bd7aeccf35dcb0fb3ba48b62f52e5f57ee52397f
Marinela Rajković <marinela.rajkovic29@gmail.com>
2025-12-30 20:36:08 +0100

    Adding eCommerce webshop

--- app.js
+++ app.js
@@ -55,6 +55,13 @@
                 image: "Mariposa.png",
                 url: "https://mariposa-kappa.vercel.app/",
             },
+
+            {
+                title: "Coffee Webshop - eCommerce website",
+                description: "The project is currently being done locally, so I am attaching a short screen video to give you an impression of the structure and appearance of the site.",
+                image: "coffeeShop.png",
+                url: "CoffeeShop.mp4",
+            },
         ];
 
     const projectsContainer = document.getElementById("projects");
